4038e0f29c0cd52140fe9c00feb7c089bee28c96
davidpyatt <52712354+davidpyatt@users.noreply.github.com>
2020-06-21 15:18:27 +0100

    Add files via upload

---
+++ qgis2web_2020_06_19-12_12_42_728073/data/ER_ResponseLocations_v5b_SCARBOROUGH_8.js
@@ -0,0 +1,8 @@
+var json_ER_ResponseLocations_v5b_SCARBOROUGH_8 = {
+"type": "FeatureCollection",
+"name": "ER_ResponseLocations_v5b_SCARBOROUGH_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "LOCATION": "Fire Station - 13-15 North Marine Rd, Scarborough YO12 7EY         No Vehicle.   ", "field_2": " ", "field_3": "YO12 7EY", "LAT": 54.286831, "LONG": -0.40113, "field_6": null, "field_7": null, "field_8": " " }, "geometry": { "type": "Point", "coordinates": [ -0.40113, 54.286831 ] } }
+]
+}
